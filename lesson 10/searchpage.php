f929bb0440c851f97c9fe6ac8c03cb1a00cf5bba
Elvisi2010 <elvisemerllahu12@gmail.com>
2025-09-23 11:40:10 -0700

    lesson 11
    lesson 11

---
+++ lesson 10/searchpage.php
@@ -0,0 +1,17 @@
+<?php
+get_header() ?>
+
+<main id="main" class="sitemain" role="main">
+    <section class="search-intro">
+        <h2> 
+            <?php _e("Search Posts","your-textdomain");?>
+        </h2>
+        <p>
+            <?php _e('Use the form below to search the site.','your-textdomain');?>
+        </p>
+    </section>
+
+    <?php get_search_form();?>
+</main>
+
+<?php get_footer();?>
